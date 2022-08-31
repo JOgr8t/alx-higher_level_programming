4ea6b05a65a548685f48144102a757194f93d230
Jonathan <jonathanKingoche@gmail.com>
2022-08-31 13:43:36 -0700

    my ncommit of a  function that adds all unique integers in a list (only once for each intege

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     sum = 0
-    unique_int_list = []
+    unique_list = []
     for a in my_list:
         if a not in unique_list:
-            unique_int_list.append(a)
-    for a in unique_int_list:
+            unique_list.append(a)
+    for a in unique_list:
         sum += a
     return (sum)
