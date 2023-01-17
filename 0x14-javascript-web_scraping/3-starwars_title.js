0925b4be8c7e0ae39154899133ef82548575fa92
JOgr8t <jonathanKingoche@gmail.com>
2023-01-17 05:49:27 -0800

    script that display the status code of a GET request

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
-// A script that prints the title of a Star Wars movie where the episode num matches a given int
-
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
+request(process.argv[2], function (error, response) {
   if (error) {
-    console.error(error);
+    console.error('error:', error);
   } else {
-    console.log(JSON.parse(body).title);
+    console.log('code:', response && response.statusCode);
   }
 });
 
