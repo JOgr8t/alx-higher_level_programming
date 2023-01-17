206e9c2033fb5042bbcce377868e4abe423203a1
JOgr8t <jonathanKingoche@gmail.com>
2023-01-17 06:02:46 -0800

    script that prints the number of movies where the character “Wedge Antilles” is present

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,19 @@
 #!/usr/bin/node
-// A script that prints the number of movies where the character Wedge Antilles is present
-
-const args = process.argv;
-let reqURL = args[2];
-let request = require('request');
-request(reqURL, function (error, response, body) {
+const request = require('request');
+const url = process.argv[2];
+let resultDict = {};
+let count = 0;
+let i = 0;
+let j = 0;
+request(url, function (
+  error, response, body) {
   if (error) {
-    console.log('error:', error); // Print the error if one occurred
+    console.log(error);
   } else {
-    let jso = JSON.parse(body);
-    let results = jso['results'];
-    let count = 0;
-    for (let i = 0; i < results.length; i++) {
-      let chars = (results[i]['characters']);
-      for (let j = 0; j < chars.length; j++) {
-        let check18 = chars[j].endsWith('18/');
-        if (check18) {
+    resultDict = JSON.parse(body);
+    for (i = 0; i < resultDict.results.length; i++) {
+      for (j = 0; j < resultDict.results[i].characters.length; j++) {
+        if (resultDict.results[i].characters[j].includes('/18/')) {
           count++;
         }
       }
